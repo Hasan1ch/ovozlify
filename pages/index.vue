5c6107a91cc9e9246da48fbc2327c2137d148b56
Hasan Kodirov <hkadirov.com@gmail.com>
2026-05-06 22:28:16 +0500

    Initial Nuxt setup with Tailwind

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="p-8 bg-blue-900">
+        <h1 class= "text-4xl font-bold text-blue-500">{{name}}</h1>
+
+        <p class="text-sm text-gray-500">{{description}}</p>
+
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+ 
+ const name = ref('Ovozlify')
+ const description = ref('Pdf to Audiobook')
+
+</script>
