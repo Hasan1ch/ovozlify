fbc58726332d36b443e5bd89713cdd9bce07903c
Hasan Kodirov <hkadirov.com@gmail.com>
2026-05-08 02:41:14 +0500

    Phase 2: Supabase connected

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss','@nuxtjs/supabase'],
   app: { head: {link: [
     {rel:"preconnect", href:"https://fonts.googleapis.com"},
     {rel:"preconnect", href:"https://fonts.gstatic.com", crossorigin:""},
     {href:"https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lora:ital,wght@0,400..700;1,400..700&display=swap", rel:"stylesheet"}
-   ]}}
+   ]}},
+  supabase:{
+    redirectOptions:{
+      login:'/login',
+      callback:'/confirm',
+      exclude:['/*']
+    }
+  }
 })
