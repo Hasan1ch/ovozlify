fd0b29d605e853c218b18cca483abab4e73cf9b8
Hasan Kodirov <hkadirov.com@gmail.com>
2026-05-10 13:29:18 +0500

    Phase 3: Login page done

---
+++ pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>Login</h1>
+    <form @submit.prevent="logIn">
+        
+        <label  for=""> <input v-model="email" type="text">Email</label>
+        <label  for=""> <input v-model="password" type="password">Password</label>
+        <button type="submit">Login</button>
+    </form>
+    
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const email = ref('')
+const password = ref('')
+const supabase = useSupabaseClient()
+const errorMessage = ref("error happened")
+
+async function logIn(){
+    const {error} = await supabase.auth.signInWithPassword({
+        email:email.value,
+        password:password.value
+    })
+    if (error){
+        errorMessage.value = error.message
+    }else {
+        navigateTo('/')
+    }
+}
+
+
+
+</script>
+
+
+<style></style>
