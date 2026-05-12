e731bafb1fa27de0c30504cc0d143ca9db808e4f
Hasan Kodirov <hkadirov.com@gmail.com>
2026-05-12 10:11:20 +0500

    Phase 4: profile page

---
+++ pages/profile.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex justify-center items-center flex-col">
+        <h1 class="text-primary text-5xl">
+            Profile
+        </h1>
+        <h1 class="text-primary text-xl m-8">Email: {{ user.email }}</h1>
+        <button type="submit" class="border rounded-xl text-secondary p-2 ">Delete account</button>
+    </div>
+
+</template>
+
+<script setup>
+const user = useSupabaseUser()
+
+
+definePageMeta({
+    middleware:'auth',
+    layout:'default'
+})
+
+
+console.log(user.value)
+</script>
+
