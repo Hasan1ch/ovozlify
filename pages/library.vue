68311a27841fb981bc8081c883026a94e14b4b43
Hasan Kodirov <hkadirov.com@gmail.com>
2026-05-14 02:29:45 +0500

    Phase 4: complete

--- pages/library.vue
+++ pages/library.vue
@@ -3,16 +3,51 @@
         <img class="invert m-2 w-16" src="/book-2.svg" />
         <h1 class="text-primary text-5xl m-2 font-sans">Your Library</h1>
         <h2 class="text-secondary text-2xl m-2 font-sans">No books yet. Upload your first PDF to get started.</h2>
-        <button class="bg-accent-terracotta p-2 rounded-xl m-2 font-sans" >Upload your first book</button>
+        <input type="file" @change="handleFileChange" id="myInput" class="bg-accent-terracotta border rounded-2xl">
+        <button type="submit" id="myInput" class="bg-accent-terracotta p-2 rounded-xl m-2 font-sans" >Upload your first book</button>
     </div>
 </template>
 
 <script setup >
+import {ref} from 'vue'
+
+
+const myInput = ref(null)
+
+function handleFileChange(event){
+    const file = event.target.files[0]
+
+    if (file.type!=="application/pdf"){
+       return console.log("Только PDF")
+    }
+
+    const formData = new FormData()
+    formData.append('file',file)
+    console.log(formData.get('file'))
+
+
+    async function uploadFile(formData){
+        
+       await fetch('/api/upload', {
+            method:'POST',
+            body: formData
+        })
+    }
+    uploadFile(formData)
+
+    console.log(event.target.files[0])
+    console.log(file.size/1024/1024)
+
+}
+
+
+
 
 definePageMeta ({
     middleware:'auth',
     layout:'default'
 })
+
 
 
 </script>
