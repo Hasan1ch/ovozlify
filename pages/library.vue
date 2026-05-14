412e13541c8c499028e158e3d180f7a45716118f
Hasan Kodirov <hkadirov.com@gmail.com>
2026-05-15 01:46:58 +0500

    reverting file upload handling in library page

--- pages/library.vue
+++ pages/library.vue
@@ -5,43 +5,29 @@
         <h2 class="text-secondary text-2xl m-2 font-sans">No books yet. Upload your first PDF to get started.</h2>
         <input type="file" @change="handleFileChange" id="myInput" class="bg-accent-terracotta border rounded-2xl">
         <button type="submit" id="myInput" class="bg-accent-terracotta p-2 rounded-xl m-2 font-sans" >Upload your first book</button>
+        <h2 class="text-secondary text-2xl"> {{ file.name }} {{ size }} MB</h2>
+        <h3 class="text-secondary text-xl">{{ pdfErrorMessage }}</h3>
     </div>
 </template>
 
 <script setup >
 import {ref} from 'vue'
-
-
-const myInput = ref(null)
+const size = ref('')
+const file = ref('')
+const pdfErrorMessage= ref('')
 
 function handleFileChange(event){
-    const file = event.target.files[0]
+    pdfErrorMessage.value= ''
+    console.log(event.target.files[0]);
+    file.value = (event.target.files[0]);
 
-    if (file.type!=="application/pdf"){
-       return console.log("Только PDF")
+
+    size.value = Math.round((event.target.files[0].size / (1024*1024) ) )
+
+    if(file.value.type!=='application/pdf'){
+        return pdfErrorMessage.value = "Только PDF"
     }
-
-    const formData = new FormData()
-    formData.append('file',file)
-    console.log(formData.get('file'))
-
-
-    async function uploadFile(formData){
-        
-       await fetch('/api/upload', {
-            method:'POST',
-            body: formData
-        })
-    }
-    uploadFile(formData)
-
-    console.log(event.target.files[0])
-    console.log(file.size/1024/1024)
-
 }
-
-
-
 
 definePageMeta ({
     middleware:'auth',
